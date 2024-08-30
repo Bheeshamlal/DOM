0ebb74bb04cb9b04a08b7dfbdd21b48a359351ef
Bheeshamlal <bheeshamlal.bscssef20@iba-suk.edu.pk>
2024-08-30 18:25:58 +0500

    getElementByTagName

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
+// // let smallImages = document.getElementsByClassName("oldImg");
+
+// // for(let i =0;i<smallImages.length;i++){
+// //     console.log(smallImages[i]);
+// //     console.dir(smallImages[i]);
+// // }
+
+// // changing below 3 images to above spider man image
 // let smallImages = document.getElementsByClassName("oldImg");
 
-// for(let i =0;i<smallImages.length;i++){
-//     console.log(smallImages[i]);
-//     console.dir(smallImages[i]);
+// for(let i = 0; i<smallImages.length; i++){
+//     smallImages[i].src = "assets/spiderman_img.png";
+//     console.log(`value of image no. ${i} is changed`);
 // }
 
-// changing below 3 images to above spider man image
-let smallImages = document.getElementsByClassName("oldImg");
+// if we want to select all 'p' tags
+// document.getElementsByTagName("p")[1].innerText= "Abc";
 
-for(let i = 0; i<smallImages.length; i++){
-    smallImages[i].src = "assets/spiderman_img.png";
-    console.log(`value of image no. ${i} is changed`);
-}
+
